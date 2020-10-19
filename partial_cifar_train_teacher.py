26a9fc74e0cb360ca3694d4acd77d99ba3aa66b2
Guy-Shapira <58084279+Guy-Shapira@users.noreply.github.com>
2020-10-19 13:04:28 +0300

    Update partial_cifar_train_teacher.py

--- partial_cifar_train_teacher.py
+++ partial_cifar_train_teacher.py
@@ -1,136 +1,3 @@
-# 2019.07.24-Changed output of forward function
-# Huawei Technologies Co., Ltd. <foss@huawei.com>
-# taken from https://github.com/huawei-noah/Data-Efficient-Model-Compression/blob/master/DAFL/resnet.py
-# for comparison with DAFL
-
-
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-
-import argparse
-
-
-class BasicBlock(nn.Module):
-    expansion = 1
-
-    def __init__(self, in_planes, planes, stride=1):
-        super(BasicBlock, self).__init__()
-        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(planes)
-
-        self.shortcut = nn.Sequential()
-        if stride != 1 or in_planes != self.expansion * planes:
-            self.shortcut = nn.Sequential(
-                nn.Conv2d(in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(self.expansion * planes)
-            )
-
-    def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = self.bn2(self.conv2(out))
-        out += self.shortcut(x)
-        out = F.relu(out)
-        return out
-
-
-class Bottleneck(nn.Module):
-    expansion = 4
-
-    def __init__(self, in_planes, planes, stride=1):
-        super(Bottleneck, self).__init__()
-        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(planes)
-        self.drop1 = nn.Dropout(p=0.55)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(planes)
-        self.drop2 = nn.Dropout(p=0.55)
-
-        self.conv3 = nn.Conv2d(planes, self.expansion * planes, kernel_size=1, bias=False)
-        self.bn3 = nn.BatchNorm2d(self.expansion * planes)
-
-        self.shortcut = nn.Sequential()
-        if stride != 1 or in_planes != self.expansion * planes:
-            self.shortcut = nn.Sequential(
-                nn.Conv2d(in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(self.expansion * planes)
-            )
-
-    def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = self.drop1(out)
-        out = F.relu(self.bn2(self.conv2(out)))
-        out = self.drop2(out)
-        out = self.bn3(self.conv3(out))
-        out += self.shortcut(x)
-        out = F.relu(out)
-        return out
-
-
-class ResNet(nn.Module):
-    def __init__(self, block, num_blocks, num_classes=10):
-        super(ResNet, self).__init__()
-        self.in_planes = 64
-
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(64)
-        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
-        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
-        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
-        self.linear = nn.Linear(512 * block.expansion, num_classes)
-
-    def _make_layer(self, block, planes, num_blocks, stride):
-        strides = [stride] + [1] * (num_blocks - 1)
-        layers = []
-        for stride in strides:
-            layers.append(block(self.in_planes, planes, stride))
-            self.in_planes = planes * block.expansion
-        return nn.Sequential(*layers)
-
-    def forward(self, x, out_feature=False):
-        x = self.conv1(x)
-
-        x = self.bn1(x)
-        out = F.relu(x)
-
-        out = self.layer1(out)
-        out = self.layer2(out)
-        out = self.layer3(out)
-        out = self.layer4(out)
-        out = F.avg_pool2d(out, 4)
-        feature = out.view(out.size(0), -1)
-        out = self.linear(feature)
-        if out_feature == False:
-            return out
-        else:
-            return out, feature
-
-
-def ResNet18(num_classes=50):
-    return ResNet(BasicBlock, [2, 2, 2, 2], num_classes)
-
-
-def ResNet34(num_classes=50):
-    return ResNet(BasicBlock, [3, 4, 6, 3], num_classes)
-
-
-def ResNet50(num_classes=50):
-    return ResNet(Bottleneck, [3, 4, 6, 3], num_classes)
-
-
-def ResNet101(num_classes=50):
-    return ResNet(Bottleneck, [3, 4, 23, 3], num_classes)
-
-
-def ResNet152(num_classes=100):
-    return ResNet(Bottleneck, [3, 8, 36, 3], num_classes)
-
-
-
-
 
 import torch
 import torchvision
@@ -139,6 +6,7 @@ import torch.nn as nn
 import tqdm
 import sys
 import os
+from ResNetCifar import ResNet18, RestNet34, ResNet50
 
 def get_split_cifar100( batch_size=32, start=0, end=50):
     shuffle = False
@@ -268,7 +136,7 @@ if __name__ == "__main__":
     parser.add_argument('--start_class', default=0, type=int, help='start class (included)')
     parser.add_argument('--end_class', default=50, type=int, help='end_class (not included)')
     parser.add_argument('--num_epochs', default=100, type=int, help='number of epochs to train')
-    parser.add_argument('--resnet', default=34,type=int,help='Resnet type of teacher model')
+    parser.add_argument('--resnet', default=34, type=int, help='Resnet type of teacher model')
     args = parser.parse_args()
 
     print("training Resnet"+str(args.resnet)+" strat_class="+str(args.start_class)+" end_class="+str(args.end_class))
@@ -278,8 +146,10 @@ if __name__ == "__main__":
     num_classes = int(100/int(args.n))
     if int(args.resnet) == 34:
         model = ResNet34(num_classes)
-    else:
+    elif int(args.resnet) == 50:
         model = ResNet50(num_classes)
+    else:
+        model = ResNet18(num_classes)
 
     if not os.path.exists("./resnet"+str(args.resnet)+"split_cifar_to" + str(args.n)):
         os.mkdir("./resnet"+str(args.resnet)+"split_cifar_to" + str(args.n))
